2d27052029cd1a3272854c65fcd19a6aed808882
Tanaka1321 <polinadaniil.2.0.0.7@gmail.com>
2026-09-14 06:49:59 +0000

    feat: add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer({ organization }) {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>© 2025 {organization}. Все права защищены.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
